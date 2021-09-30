808879f13b3e381dda5bd80b5a1b24bc56e5aa37
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-30 08:03:32 +0800

    add forms in modal

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,10 @@ const App: React.FC = () => {
         <Button addNotes = {addNotes} btnTxt = "ADD NOTE"/>
       </div>
       <Modal trigger = {popUp} click = {close}>
-       <form action="">
-         <input type="text" id ='title' placeholder = 'Title' className ='border-[1px] border-black' />
-         <input type="text" id ='title' placeholder = 'Description' />
+       <form className = 'w-80 h-96 flex flex-col gap-y-3 align-center items-center justify-center'>
+         <input type="text" id ='title' placeholder = 'Title' className ='w-[93%] border-[1px] border-gray-600' />
+         <textarea name="description" id="desc" className = 'border-[1px] border-gray-600' cols = '40' rows = "8" placeholder = 'Description'></textarea>
+         <button type = 'submit' className = 'w-20 rounded-md block font-semibold bg-gradient-to-r from-green-400 to-blue-500'>Add</button>
        </form>
       </Modal>
       {/* Notes Container*/}
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import {CgClose} from 'react-icons/cg'
 const Modal:React.FC<modal|null> = ({children,trigger,click}) => {
     return trigger? (
-      <div className = 'bg-white w-80 h-96 mx-auto rounded-[2rem]'>
-        <CgClose className = 'cursor-pointer pt-4 ml-2 w-7 h-auto fill-current text-red-800'
+      <div className = 'bg-white w-80 h-96 mx-auto rounded-[2rem] relative'>
+        <CgClose className = 'cursor-pointer pt-4 ml-2 w-6 h-auto fill-current text-red-800 absolute right-4'
         onClick = {click}/>
         {children}
       </div>
