152991cbae2ef6f2262817dcd9fcc2a49046e8db
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-02 08:01:38 +0800

    Add font -family ```Caveat```

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import {RiEditBoxLine,RiDeleteBin6Line} from 'react-icons/ri'
 
 const Notes:React.FC<Notes> = ({title,description}) => {
     return (
-        <div className = "w-52 h-52 bg-yellow-300 rounded-3xl space-x-2 space-y-2">
-            <h3 className = 'text-center mt-1 font-semibold'>{title}</h3>
+        <div className = "font-primary text-xl w-52 h-52 bg-yellow-300 rounded-3xl space-x-2 space-y-2">
+            <h3 className = 'mt-2 ml-2 font-bold'>{title}</h3>
             <p>{description}</p>
+            <div className = 'flex'>
+                <RiEditBoxLine/>
+                <RiDeleteBin6Line/>
+            </div>
         </div>
     )
 } 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: "'Caveat', cursive"
+      }
+    },
   },
   variants: {
     extend: {},
