38e1e5f83e5c2c51e2dffa4c9dd09a3c01f3539b
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-07 12:51:56 +0800

    Improve UI

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,11 +5,11 @@ import {CgClose} from 'react-icons/cg'
 const Modal:React.FC<modal|null> = ({refTitle,refDesc,trigger,click,handleSubmit,titleChange,descChange,titleValue,descValue,submitBtnTxt}) => {
     return trigger?(
       <div className = 'bg-[#C0C0C0] bg-opacity-95 w-80 h-96 mx-auto rounded-[2rem] absolute left-0 right-0 mx-auto z-50'>
-        <CgClose className = 'cursor-pointer pt-4 ml-2 w-6 h-auto fill-current text-red-800 absolute right-4'
+        <CgClose className = 'cursor-pointer pt-4 ml-2 w-6 h-auto fill-current text-black absolute right-4'
         onClick = {click}/>
         <form onSubmit = {handleSubmit} className = 'font-tertiary text-lg w-80 h-96 flex flex-col gap-y-3 align-center items-center justify-center'>
           <input type="text" ref = {refTitle} onChange = {titleChange} value = {titleValue} name = 'text' id ='title' placeholder = 'Title' className ='w-[93%] rounded-md p-1 mt-8' required />
-          <textarea name="description" ref = {refDesc}  onChange = {descChange} value = {descValue} id="desc" className = 'rounded-md p-1' cols = {39} rows = {8} placeholder = 'Description' required></textarea>
+          <textarea name="description" ref = {refDesc}  onChange = {descChange} value = {descValue} id="desc" className = 'rounded-md p-1 w-[93%]'  cols = {39} rows = {8} placeholder = 'Description' required></textarea>
           <button type = 'submit' className = 'text-white bg-[#373736] opacity-7 font-primary w-20 rounded-md block font-semibold'>{submitBtnTxt}</button>
         </form>
       </div>
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -3,7 +3,7 @@ import {RiEditBoxLine,RiDeleteBin6Line} from 'react-icons/ri'
 
 const Notes:React.FC<Notes> = ({title,description,removeNote,editNote}) => {
     return (
-        <div className = "relative font-tertiary text-xl w-48 h-48 p-1 md:w-52 md:h-52 bg-[#F3F369] space-2 z-10 drop-shadow-xl " id = "note">
+        <div className = "relative font-tertiary text-xl w-[10.5rem] h-[10.5rem] p-1 md:w-52 md:h-52 bg-[#F3F369] space-2 z-10 drop-shadow-xl " id = "note">
             <h3 className = 'mt-2 ml-2 font-bold text-lg md:text-xl'>{title}</h3>
             <p className = 'text-base md:text-xl ml-2'>{description}</p>
             <div className = 'absolute bottom-2 left-1 flex gap-x-1'>
