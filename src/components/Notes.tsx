d9079e11b6abfa0dc2976b27f5537433a4a5477f
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-11 21:31:55 +0800

    Add wrapper for text if overflow

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -4,8 +4,8 @@ import {RiEditBoxLine,RiDeleteBin6Line} from 'react-icons/ri'
 const Notes:React.FC<Notes> = ({title,description,removeNote,editNote}) => {
     return (
         <div className = "relative font-tertiary text-xl w-[10.5rem] h-[10.5rem] p-1 md:w-52 md:h-52 bg-[#F3F369] space-2 z-10 drop-shadow-xl " id = "note">
-            <h3 className = 'mt-2 ml-2 font-bold text-lg md:text-xl'>{title}</h3>
-            <p className = 'text-base md:text-xl ml-2'>{description}</p>
+            <h3 className = 'mt-2 ml-2 font-bold text-lg md:text-xl overflow-ellipsis w-[10.2rem] md:w-[12.6rem]'>{title}</h3>
+            <p className = 'text-sm md:text-base ml-2 overflow-ellipsisw w-[9.8rem] md:w-[12.6rem]'>{description}</p>
             <div className = 'absolute bottom-2 left-1 flex gap-x-1'>
                 <RiEditBoxLine className = 'cursor-pointer' onClick = {editNote}/>
                 <RiDeleteBin6Line className = 'cursor-pointer' onClick = {removeNote}/>
