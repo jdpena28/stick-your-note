e45d1d7b1ed852b39e2311106026bc9815829c1d
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-21 16:15:05 +0800

    Change description for how to edit notes

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
     {
       id: 2,
       title: 'How to edit Notes?',
-      desc: 'Double Click the edit icon to edit text content'
+      desc: 'Click the edit icon to edit text content'
     }
 ])
   const addNotes = (id?:number,title?:string,desc?:string) => {
