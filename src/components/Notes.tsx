17f613ea057ca8dc14c884c36f6b55e432e4728e
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-23 09:22:18 +0800

    Add onMount animations

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import {motion,AnimatePresence} from 'framer-motion'
 import {RiEditBoxLine,RiDeleteBin6Line} from 'react-icons/ri'
 
 const Notes:React.FC<Notes> = ({title,description,removeNote,editNote}) => {
     return (
-        <div className = "font-tertiary text-xl w-[10.5rem] h-[10.5rem] p-1 md:w-52 md:h-52 bg-[#F3F369] space-2 z-10 drop-shadow-xl " id = "note">
-            <h3 className = 'mt-2 ml-2 font-bold text-lg md:text-xl overflow-ellipsis w-[10.2rem] md:w-[12.6rem] whitespace-pre-line overflow-x-hidden'>{title}</h3>
-            <p className = 'text-sm md:text-base ml-2 overflow-ellipsis overflow-x-hidden whitespace-pre-line w-[9.8rem] md:w-[12.6rem]'>{description}</p>
-            <div className = 'absolute bottom-1 left-1 flex gap-x-1 '>
-                <RiEditBoxLine className = 'cursor-pointer' size={'1.2rem'} onClick = {editNote}/>
-                <RiDeleteBin6Line className = 'cursor-pointer' size={'1.2rem'} onClick = {removeNote}/>
-            </div>
-        </div>
+            <motion.div 
+            initial = {{x:'100vw'}}
+            animate ={{x:0}}
+            transition ={{type: 'spring',duration:1,bounce:0.2,delay:0.3}}
+            className = "font-tertiary text-xl w-[10.5rem] h-[10.5rem] p-1 md:w-52 md:h-52 bg-[#F3F369] space-2 z-10 drop-shadow-xl " id = "note">
+                <h3 className = 'mt-2 ml-2 font-bold text-lg md:text-xl overflow-ellipsis w-[10.2rem] md:w-[12.6rem] whitespace-pre-line overflow-x-hidden'>{title}</h3>
+                <p className = 'text-sm md:text-base ml-2 overflow-ellipsis overflow-x-hidden whitespace-pre-line w-[9.8rem] md:w-[12.6rem]'>{description}</p>
+                <div className = 'absolute bottom-1 left-1 flex gap-x-1 '>
+                    <RiEditBoxLine className = 'cursor-pointer' size={'1.2rem'} onClick = {editNote}/>
+                    <RiDeleteBin6Line className = 'cursor-pointer' size={'1.2rem'} onClick = {removeNote}/>
+                </div>
+            </motion.div>
     )
 } 
 export default Notes
